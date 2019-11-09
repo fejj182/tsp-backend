73769a7c6ac4ceb208a25397d3d94d2204a8d521
Jeff Zinger <jzinger100@gmail.com>
2019-11-09 08:37:03 +0100

    TSP-1: Get nearest station by lat and lon

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Station;
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
 class StationController extends Controller
@@ -11,5 +12,25 @@ class StationController extends Controller
     public function index(): Collection
     {
         return Station::all();
+    }
+
+    public function nearest(Request $request): Collection
+    {
+        $lat = $request->input('lat');
+        $lon = $request->input('lon');
+        return Station::query()
+        ->selectRaw('*, (
+            3959 *
+            acos(cos(radians(?)) * 
+            cos(radians(lat)) * 
+            cos(radians(lon) - 
+            radians(?)) + 
+            sin(radians(?)) * 
+            sin(radians(lat )))
+         ) AS distance', [$lat, $lon, $lat])
+        ->where('enabled', 1)
+        ->orderBy('distance', 'asc')
+        ->take(1)
+        ->get();
     }
 }
