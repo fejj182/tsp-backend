3d40aceae922841e81df50a9b32a3bed64f37f93
Jeff Zinger <jzinger100@gmail.com>
2019-11-21 19:30:09 +0100

    TSP-1: Test endpoints using factories and test db

---
+++ database/factories/StationFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Illuminate\Support\Str;
+use App\Models\Station;
+use Faker\Generator as Faker;
+
+$factory->define(Station::class, function (Faker $faker) {
+    return [
+        'station_id' => Str::random(5),
+        'name' => $faker->city,
+        'lat' => $faker->latitude,
+        'lon' => $faker->longitude,
+        'enabled' => $faker->boolean,
+    ];
+});
--- tests/Feature/EndpointsTest.php
+++ tests/Feature/EndpointsTest.php
@@ -2,13 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\Station;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class EndpointsTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testStations()
     {
+        factory(Station::class)->create([
+            'name' => 'Barcelona-Sants',
+        ]);
         $response = $this->get('/api/stations');
         $response->assertJsonFragment(["name" => "Barcelona-Sants"]);
         $response->assertStatus(200);
@@ -16,6 +22,14 @@ class EndpointsTest extends TestCase
 
     public function testStationsNearest()
     {
+        factory(Station::class)->create([
+            'name' => 'Barcelona-Sants',
+        ]);
+        factory(Station::class)->create([
+            'name' => 'Valencia-Estacio del Nord',
+            'lat' => '39.465064',
+            'lon' => '-0.377433'
+        ]);
         $response = $this->post('/api/stations/nearest', ["lat" => "39.465064", "lon" => "-0.377433"]);
         $response->assertJsonFragment(["name" => "Valencia-Estacio del Nord"]);
         $response->assertStatus(200);
