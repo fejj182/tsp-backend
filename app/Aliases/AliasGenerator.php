aff97a9eb119cfe6784daae6e6fd676326d9d871
Jeff Zinger <jzinger100@gmail.com>
2020-02-03 17:51:48 +0100

    TSP-10: Add alias generator

---
+++ app/Aliases/AliasGenerator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Aliases;
+
+use SplFileObject;
+
+class AliasGenerator
+{
+    public static function generate()
+    {
+        $parts = array();
+
+        $file = new SplFileObject(__DIR__ . '/short_words.txt');
+
+        for ($i = 0; $i < 2; $i++) {
+            $lineNumber = random_int(0, 1295);
+            $file->seek($lineNumber);
+            $parts[] = trim($file->current());
+        }
+
+        $parts[] = random_int(100,999);
+
+        return implode((string) '-', $parts);
+    }
+}
