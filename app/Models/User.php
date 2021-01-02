e7d0a01b20f70a81cf3d943509efd5d099d1cc9c
Jeff <jzinger100@gmail.com>
2021-01-02 10:07:06 +0000

    TSP-6-17: Add default admin attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'admin'
     ];
 
     /**
@@ -35,5 +35,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'admin' => false
     ];
 }
