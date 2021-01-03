f49acdc212c7b3c65489a73b3021f91f1a2d4015
Jeff <jzinger100@gmail.com>
2021-01-03 10:39:20 +0000

    TSP-6-18: Text ExampleComponentt

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ import vuetify from './plugins/vuetify.js'
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-componentt', require('./components/ExampleComponentt.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/ExampleComponentt.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <v-card>
+                    <v-card-title>
+                        Example Componentt
+                    </v-card-title>
+                    <v-card-text>
+                        I'm a Vuetify example component
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-autocomplete label="Type here..." :items="items" filled rounded></v-autocomplete>
+                    </v-card-actions>
+                </v-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: ['foo', 'bar', 'fizz', 'buzz']
+        }),
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
